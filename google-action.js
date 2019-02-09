f54cf5c13c41be6b1537378e19a1f4a95742bc73
Markus Korporal <markus@korporal.de>
2019-02-09 21:50:00 +0100

    Support for basicCard, browseCarousel and carousel

--- google-action.js
+++ google-action.js
@@ -39,7 +39,6 @@ module.exports = function(RED) {
     // ActionsSdkApp can't be cloned so we need to keep a central copy.
 
     var appMap = new Map();
-
     function GoogleActionIn(n) {
         RED.nodes.createNode(this,n);
 
@@ -114,23 +113,92 @@ module.exports = function(RED) {
     function GoogleActionOut(n) {
         RED.nodes.createNode(this,n);
         var node = this;
-
         this.on("input",function(msg) {
 
             var app = appMap.get(msg.conversationId);
 
             if (app) {
-                if (msg.closeConversation) {
-                    app.tell(msg.payload.toString());
-                    appMap.delete(msg.conversationId);
-                } else {
-                    if (Array.isArray(msg.payload)) {
-                        app.ask(app.buildInputPrompt(msg.payload[0].startsWith("<speak>"), msg.payload[0],
-                                             msg.payload.slice(1,4)), msg.dialogState);
-                    } else {
-                        app.ask(msg.payload.toString(), msg.dialogState);
-                    }
-                }
+				try {
+					if (typeof msg.payload === 'object' && typeof msg.payload.carousel == "undefined" && !Array.isArray(msg.payload)){
+						var response=app.buildRichResponse();
+						if (typeof msg.payload.basicCard === 'object'){
+							var basicCard=app.buildBasicCard();
+							if (msg.payload.basicCard.title) basicCard.setTitle(msg.payload.basicCard.title);
+							if (msg.payload.basicCard.bodyText) basicCard.setBodyText(msg.payload.basicCard.bodyText);
+							if (msg.payload.basicCard.image) basicCard.setImage(msg.payload.basicCard.image.url,msg.payload.basicCard.image.title);
+							if (msg.payload.basicCard.button)basicCard.addButton(msg.payload.basicCard.button.title, msg.payload.basicCard.button.url);
+							if (msg.payload.basicCard.imageDisplay=='white')
+								basicCard.setImageDisplay(app.ImageDisplays.WHITE)
+							else if (msg.payload.basicCard.imageDisplay=='default')
+								basicCard.setImageDisplay(app.ImageDisplays.DEFAULT);
+							response.addBasicCard(basicCard);
+						}
+						if (typeof msg.payload.browseCarousel === 'object'){
+							var browseCarousel=app.buildBrowseCarousel();	
+							var i;
+							for (i=0;i<msg.payload.browseCarousel.items.length;i++){
+								var item=app.buildBrowseItem();
+								if (msg.payload.browseCarousel.items[i].title)item.setTitle(msg.payload.browseCarousel.items[i].title);
+								if (msg.payload.browseCarousel.items[i].description)item.setDescription(msg.payload.browseCarousel.items[i].description);
+								if (msg.payload.browseCarousel.items[i].footerText)item.setFooter(msg.payload.browseCarousel.items[i].footerText);
+								if (msg.payload.browseCarousel.items[i].image) item.setImage(msg.payload.browseCarousel.items[i].image.url,msg.payload.browseCarousel.items[i].image.title);
+								if (msg.payload.browseCarousel.items[i].openUrlAction) item.setOpenUrlAction(msg.payload.browseCarousel.items[i].openUrlAction.url,msg.payload.browseCarousel.items[i].openUrlAction.urlTypeHint);
+								if (msg.payload.browseCarousel.items[i].url) item.setUrl(msg.payload.browseCarousel.items[i].url);
+								if (msg.payload.browseCarousel.items[i].urlTypeHint) item.setUrlTypeHint(msg.payload.browseCarousel.items[i].urlTypeHint);								
+								browseCarousel.addItems(item);
+							}
+							if (msg.payload.browseCarousel.imageDisplay=='white')
+								browseCarousel.setImageDisplay(app.ImageDisplays.WHITE)
+							else if (msg.payload.browseCarousel.imageDisplay=='default')
+								browseCarousel.setImageDisplay(app.ImageDisplays.DEFAULT);							
+							response.addBrowseCarousel(browseCarousel);
+						}
+						if (msg.payload.simpleResponse) response.addSimpleResponse(msg.payload.simpleResponse);
+						if (msg.payload.suggestion)response.addSuggestions(msg.payload.suggestion);							
+						if (msg.closeConversation) {
+							app.tell(response);
+							appMap.delete(msg.conversationId);
+						} else {
+							app.ask(response, msg.dialogState)
+						}
+					} else if (typeof msg.payload === 'object' && typeof msg.payload.carousel != "undefined"){
+						var carousel=app.buildCarousel ();
+						var i;
+						for (i=0;i<msg.payload.carousel.items.length;i++){
+							var item=app.buildOptionItem();
+							if (msg.payload.carousel.items[i].title)item.setTitle(msg.payload.carousel.items[i].title);
+							if (msg.payload.carousel.items[i].description)item.setDescription(msg.payload.carousel.items[i].description);
+							if (msg.payload.carousel.items[i].image) item.setImage(msg.payload.carousel.items[i].image.url,msg.payload.carousel.items[i].image.title);
+							if (msg.payload.carousel.items[i].key) item.setKey(msg.payload.carousel.items[i].key);
+							if (msg.payload.carousel.items[i].synonyms) item.addSynonyms(msg.payload.carousel.items[i].synonyms);								
+							carousel.addItems(item);
+						}				
+						if (msg.payload.carousel.imageDisplay=='white')
+							carousel.setImageDisplay(app.ImageDisplays.WHITE)
+						else if (msg.payload.carousel.imageDisplay=='default')
+							carousel.setImageDisplay(app.ImageDisplays.DEFAULT);							
+						
+						app.askWithCarousel (msg.payload.simpleResponse, carousel, msg.dialogState)
+					} else if (Array.isArray(msg.payload)) {
+						if (msg.closeConversation) {
+							app.tell(app.buildInputPrompt(msg.payload[0].startsWith("<speak>"), msg.payload[0],msg.payload.slice(1,4)));
+							appMap.delete(msg.conversationId);
+						} else {														
+							app.ask(app.buildInputPrompt(msg.payload[0].startsWith("<speak>"), msg.payload[0],
+											 msg.payload.slice(1,4)), msg.dialogState);							
+						}
+					} else {
+						if (msg.closeConversation) {
+							app.tell(msg.payload.toString());
+							appMap.delete(msg.conversationId);
+						} else {							
+							app.ask(msg.payload.toString(), msg.dialogState);
+						}
+					}
+				} catch (err) {
+					node.warn("exception occured:",err);
+				}
+					
             } else {
                 node.warn("Invalid conversation id");
             }
