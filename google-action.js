6b80f379b2b3771f5d83db978f6b6d528a9141fc
Markus Korporal <markus@korporal.de>
2019-02-14 14:16:04 +0100

    Upgrade to API Version 2

--- google-action.js
+++ google-action.js
@@ -27,8 +27,16 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 module.exports = function(RED) {
     "use strict";
 
-    const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
-
+	const {
+	 actionssdk,
+	 Image,
+	 Table,
+	 Carousel,
+	 BrowseCarousel,
+	 BasicCard,
+	 Button,
+	 SimpleResponse
+	}= require('actions-on-google');
     const express = require('express');
     const https = require("https");
     const fs = require('fs');
@@ -38,7 +46,10 @@ module.exports = function(RED) {
     // Map of app handlers
     // ActionsSdkApp can't be cloned so we need to keep a central copy.
 
-    var appMap = new Map();
+    const app = actionssdk();
+	
+	var conversations=[];
+	
     function GoogleActionIn(n) {
         RED.nodes.createNode(this,n);
 
@@ -48,59 +59,189 @@ module.exports = function(RED) {
         node.port = n.port || 8081;
         node.key = n.key || '';
         node.cert = n.cert || '';
-
-        const options = {
+		node.defaultSimpleResponse = n.defaultSimpleResponse || 'I send you the following information';
+		node.noAnswerReceivedMsg = n.noAnswerReceived || 'I have no valid Response for you. Sorry!';
+		node.exceptionMsg = n.exceptionMsg || 'I encountered a glitch. Can you say that again?';
+		const options = {
             key: fs.readFileSync(node.key),
             cert: fs.readFileSync(node.cert)
         };
+   
 
-                // Create new http server to listen for requests
+       // Handler for requests	
+		app.fallback((conv, params, option) => {
+
+			var msg = {topic: node.topic,
+						conversationId: conv.id,
+						surface: conv.surface,
+						intent: conv.intent,
+						dialogState: conv.data,
+						closeConversation: true,
+						option:option
+					};
+
+			msg.userId = (conv.user ? conv.user.id : 0);
+			msg.locale = (conv.user ? conv.user.locale : "");
+
+			switch(msg.intent) {
+				case 'actions.intent.OPTION':
+					msg.payload = conv.input;
+					break;
+				default:
+					msg.payload = conv.input.raw;
+			}
+
+			node.send(msg);
+			var flowContext = this.context().flow;
+			node.trace("request: " + msg.payload);
+			var timer;
+			return new Promise(function (resolve, reject) {
+				conversations [conv.id]=resolve;
+				timer=setTimeout(() => { 
+					reject() }, 5000);
+                }).then((msg) => {	
+					clearTimeout(timer);						
+					conversations=conversations.filter(function(ele){return ele != conv.id;});
+					conv.data=msg.dialogState;
+					try {							
+
+						if (typeof msg.payload === 'object'){
+							// Simple Response is mandatory
+							if (msg.payload.simpleResponse){
+								var simpleResponse;
+								node.trace("simple Response: " + JSON.stringify (msg.payload.simpleResponse));
+								if (typeof msg.payload.simpleResponse === 'string'){
+									simpleResponse=new SimpleResponse(msg.payload.simpleResponse);
+								} else {									
+									simpleResponse=new SimpleResponse(msg.payload.simpleResponse);
+									if (msg.payload.simpleResponse.speech.startsWith("<speak>")){
+										simpleResponse.ssml=msg.payload.simpleResponse.speech;
+										delete simpleResponse.textToSpeech;
+									}
+								}
+							} else {
+								simpleResponse=new SimpleResponse(node.defaultSimpleResponse);
+							}
+							if (msg.closeConversation &&!(typeof msg.payload.basicCard === 'object' || typeof msg.payload.image === 'object' && typeof msg.payload.browseCarousel === 'object' && typeof msg.payload.carousel === 'object')){
+								conv.close(simpleResponse);
+							} else {
+								conv.ask(simpleResponse);									
+							}
+							// send only one complex object
+							if (typeof msg.payload.basicCard === 'object'){
+								node.trace("Prepare basicCard");							
+								var basicCard={};						
+								if (msg.payload.basicCard.title) basicCard.title=msg.payload.basicCard.title;
+								if (msg.payload.basicCard.formattedText) basicCard.text=msg.payload.basicCard.formattedText;
+								if (msg.payload.basicCard.image) basicCard.image=new Image({url: msg.payload.basicCard.image.url,alt:msg.payload.basicCard.image.alt});
+								if (msg.payload.basicCard.button){
+									if (Array.isArray(msg.payload.basicCard.button)){
+										var i;
+										var buttons=[];
+										for (i=0;i<msg.payload.basicCard.button.length;i++){
+											buttons.push(new Button({title:msg.payload.basicCard.button[i].title,url: msg.payload.basicCard.button[i].url}));											
+										}
+										basicCard.buttons=buttons;
+									} else {
+										buttons=new Button({title:msg.payload.basicCard.button.title,url: msg.payload.basicCard.button.url});
+									}
+								}
+								if (msg.payload.basicCard.display)basicCard.display=msg.payload.basicCard.display;
+								node.trace("Add basicCard" + JSON.stringify (new BasicCard(basicCard)));													
+								if (msg.closeConversation){
+									conv.close(new BasicCard(basicCard));	
+								} else {
+									conv.ask(new BasicCard(basicCard));
+								}									
+							} else if (typeof msg.payload.image === 'object'){
+								node.trace("Prepare image");
+								if (msg.closeConversation){
+									conv.close(new Image({url: msg.payload.image.url,alt:msg.payload.image.alt}));
+								} else {
+									conv.ask(new Image({url: msg.payload.image.url,alt:msg.payload.image.alt}));
+								}
+								
+							} else if (typeof msg.payload.browseCarousel === 'object'){
+								var browseCarousel=new BrowseCarousel();	
+								var i;
+								browseCarousel.items=[];
+								for (i=0;i<msg.payload.browseCarousel.items.length;i++){
+									var item=new BrowseCarouselItem();
+									if (msg.payload.browseCarousel.items[i].title)item.title=msg.payload.browseCarousel.items[i].title;
+									if (msg.payload.browseCarousel.items[i].description)item.description=msg.payload.browseCarousel.items[i].description;
+									if (msg.payload.browseCarousel.items[i].footer)item.footer=msg.payload.browseCarousel.items[i].footer;
+									if (msg.payload.browseCarousel.items[i].image) item.image=new Image ({url: msg.payload.browseCarousel.items[i].image.url,alt: msg.payload.browseCarousel.items[i].image.alt});
+									if (msg.payload.browseCarousel.items[i].openUrlAction) item.setOpenUrlAction=msg.payload.browseCarousel.items[i].openUrlAction;
+									browseCarousel.items.push(item);
+								}														
+								if (msg.payload.browseCarousel.display=='white')browseCarousel.display=msg.payload.browseCarousel.display;
+								// Never close conversation when browseCarousel
+								conv.ask (new BrowseCarousel(browseCarousel));
+							} else if (typeof msg.payload.carousel === 'object'){
+								var carousel={items:{}};
+								var i;
+								for (i=0;i<msg.payload.carousel.items.length;i++){
+									var item={};
+									if (msg.payload.carousel.items[i].title)item.title=msg.payload.carousel.items[i].title;
+									if (msg.payload.carousel.items[i].description)item.description=msg.payload.carousel.items[i].description;
+									if (msg.payload.carousel.items[i].image) item.image=new Image({url: msg.payload.carousel.items[i].image.url,alt:msg.payload.carousel.items[i].image.alt});
+									
+									if (msg.payload.carousel.items[i].synonyms) item.synonyms=msg.payload.carousel.items[i].synonyms;	
+									if (msg.payload.carousel.items[i].key) 
+										carousel.items[msg.payload.carousel.items[i].key]=item;
+									else
+										carousel.items['item_'+i]=item;		
+								}
+								if (msg.payload.carousel.display)basicCard.display=msg.payload.carousel.display;									
+								node.trace("Add Carousel" + JSON.stringify (new Carousel(carousel)));	
+								if (msg.closeConversation){
+									conv.close (new Carousel(carousel));
+								} else {
+									conv.ask (new Carousel(carousel));
+								}
+							}
+							// suggestions are not allowed if collection will be closed
+							if (msg.closeConversation && msg.payload.suggestion)	{						
+								conv.ask (new Suggestions(msg.payload.suggestion));
+							}
+						} else {
+							if (msg.closeConversation) {
+								node.trace("Send Close " + msg.payload.toString());									
+								conv.close(msg.payload.toString());
+							} else {							
+								conv.ask(msg.payload.toString(),);
+							}
+						}
+
+					} catch (err) {
+						node.warn("exception occured: " + JSON.stringify(err) + " Msg: " + JSON.stringify(msg));
+					}
+													
+			}).catch((err) => {
+				conv.ask(node.noAnswerReceived);
+				console.log(err); //undefined
+			});
+		
+		});
+		app.catch((conv, error) => {
+		  console.error(error);
+		  conv.ask(node.exceptionMsg);
+		});		
+        // Create new http server to listen for requests
         var expressApp = express();
         expressApp.use(bodyParser.json({ type: 'application/json' }));
-        node.httpServer = https.createServer(options, expressApp);
-
-        // Handler for requests
-        expressApp.all(node.url, (request, response) => {
-
-            var app = new ActionsSdkApp({ request, response });
-            app.handleRequest(function() {
-
-                appMap.set(app.getConversationId(), app);
-                var msg = {topic: node.topic,
-                            conversationId: app.getConversationId(),
-                            intent: app.getIntent(),
-                            dialogState: app.getDialogState(),
-                            closeConversation: true,
-                        };
-		    
-                var user = app.getUser();
-                msg.userId = (user ? user.userId : 0);
-                msg.locale = (user ? user.locale : "");
-
-                switch(msg.intent) {
-                    case 'actions.intent.OPTION':
-                        msg.payload = app.getSelectedOption();
-                        break;
-                    default:
-                        msg.payload = app.getRawInput();
-                }
-
-
-                node.send(msg);
-
-                node.trace("request: " + msg.payload);
-
-            });
-        });
+		expressApp.post(node.url, app);
+        				
+		node.httpServer = https.createServer(options, expressApp);
 
         // Start listening
+		
         node.httpServer.listen(node.port);
 
         node.log("Listening on port " + node.port);
 
         // Stop listening
         node.on('close', function(done) {
-            appMap.clear();
             node.httpServer.close(function(){
                 done();
             });
@@ -114,99 +255,8 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         var node = this;
         this.on("input",function(msg) {
-
-            var app = appMap.get(msg.conversationId);
-
-            if (app) {
-				try {
-					if (typeof msg.payload === 'object' && typeof msg.payload.carousel == "undefined" && !Array.isArray(msg.payload)){
-						var response=app.buildRichResponse();
-						if (typeof msg.payload.basicCard === 'object'){
-							var basicCard=app.buildBasicCard();
-							if (msg.payload.basicCard.title) basicCard.setTitle(msg.payload.basicCard.title);
-							if (msg.payload.basicCard.bodyText) basicCard.setBodyText(msg.payload.basicCard.bodyText);
-							if (msg.payload.basicCard.image) basicCard.setImage(msg.payload.basicCard.image.url,msg.payload.basicCard.image.title);
-							if (msg.payload.basicCard.button){
-								var i;
-								for (i=0;i<msg.payload.basicCard.button.length;i++){								
-									basicCard.addButton(msg.payload.basicCard.button[i].title, msg.payload.basicCard.button[i].url);
-								}
-							}
-							if (msg.payload.basicCard.imageDisplay=='white')
-								basicCard.setImageDisplay(app.ImageDisplays.WHITE)
-							else if (msg.payload.basicCard.imageDisplay=='default')
-								basicCard.setImageDisplay(app.ImageDisplays.DEFAULT);
-							response.addBasicCard(basicCard);
-						}
-						if (typeof msg.payload.browseCarousel === 'object'){
-							var browseCarousel=app.buildBrowseCarousel();	
-							var i;
-							for (i=0;i<msg.payload.browseCarousel.items.length;i++){
-								var item=app.buildBrowseItem();
-								if (msg.payload.browseCarousel.items[i].title)item.setTitle(msg.payload.browseCarousel.items[i].title);
-								if (msg.payload.browseCarousel.items[i].description)item.setDescription(msg.payload.browseCarousel.items[i].description);
-								if (msg.payload.browseCarousel.items[i].footerText)item.setFooter(msg.payload.browseCarousel.items[i].footerText);
-								if (msg.payload.browseCarousel.items[i].image) item.setImage(msg.payload.browseCarousel.items[i].image.url,msg.payload.browseCarousel.items[i].image.title);
-								if (msg.payload.browseCarousel.items[i].openUrlAction) item.setOpenUrlAction(msg.payload.browseCarousel.items[i].openUrlAction.url,msg.payload.browseCarousel.items[i].openUrlAction.urlTypeHint);
-								if (msg.payload.browseCarousel.items[i].url) item.setUrl(msg.payload.browseCarousel.items[i].url);
-								if (msg.payload.browseCarousel.items[i].urlTypeHint) item.setUrlTypeHint(msg.payload.browseCarousel.items[i].urlTypeHint);								
-								browseCarousel.addItems(item);
-							}
-							if (msg.payload.browseCarousel.imageDisplay=='white')
-								browseCarousel.setImageDisplay(app.ImageDisplays.WHITE)
-							else if (msg.payload.browseCarousel.imageDisplay=='default')
-								browseCarousel.setImageDisplay(app.ImageDisplays.DEFAULT);							
-							response.addBrowseCarousel(browseCarousel);
-						}
-						if (msg.payload.simpleResponse) response.addSimpleResponse(msg.payload.simpleResponse);
-						if (msg.payload.suggestion)response.addSuggestions(msg.payload.suggestion);							
-						if (msg.closeConversation) {
-							app.tell(response);
-							appMap.delete(msg.conversationId);
-						} else {
-							app.ask(response, msg.dialogState)
-						}
-					} else if (typeof msg.payload === 'object' && typeof msg.payload.carousel != "undefined"){
-						var carousel=app.buildCarousel ();
-						var i;
-						for (i=0;i<msg.payload.carousel.items.length;i++){
-							var item=app.buildOptionItem();
-							if (msg.payload.carousel.items[i].title)item.setTitle(msg.payload.carousel.items[i].title);
-							if (msg.payload.carousel.items[i].description)item.setDescription(msg.payload.carousel.items[i].description);
-							if (msg.payload.carousel.items[i].image) item.setImage(msg.payload.carousel.items[i].image.url,msg.payload.carousel.items[i].image.title);
-							if (msg.payload.carousel.items[i].key) item.setKey(msg.payload.carousel.items[i].key);
-							if (msg.payload.carousel.items[i].synonyms) item.addSynonyms(msg.payload.carousel.items[i].synonyms);								
-							carousel.addItems(item);
-						}				
-						if (msg.payload.carousel.imageDisplay=='white')
-							carousel.setImageDisplay(app.ImageDisplays.WHITE)
-						else if (msg.payload.carousel.imageDisplay=='default')
-							carousel.setImageDisplay(app.ImageDisplays.DEFAULT);							
-						
-						app.askWithCarousel (msg.payload.simpleResponse, carousel, msg.dialogState)
-					} else if (Array.isArray(msg.payload)) {
-						if (msg.closeConversation) {
-							app.tell(app.buildInputPrompt(msg.payload[0].startsWith("<speak>"), msg.payload[0],msg.payload.slice(1,4)));
-							appMap.delete(msg.conversationId);
-						} else {														
-							app.ask(app.buildInputPrompt(msg.payload[0].startsWith("<speak>"), msg.payload[0],
-											 msg.payload.slice(1,4)), msg.dialogState);							
-						}
-					} else {
-						if (msg.closeConversation) {
-							app.tell(msg.payload.toString());
-							appMap.delete(msg.conversationId);
-						} else {							
-							app.ask(msg.payload.toString(), msg.dialogState);
-						}
-					}
-				} catch (err) {
-					node.warn("exception occured:",err);
-				}
-					
-            } else {
-                node.warn("Invalid conversation id");
-            }
+			//this.context().flow.set ("response",msg);
+			conversations[msg.conversationId](msg);
         });
     }
     RED.nodes.registerType("google-action response",GoogleActionOut);
